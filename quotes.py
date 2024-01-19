d0b8ced1c44a33c075022bd81efcba108192d48f
Nursultan Adiev <70845085+itsNur@users.noreply.github.com>
2024-01-19 16:31:50 +0300

    Update quotes.py
    corrected syntax

--- quotes.py
+++ quotes.py
@@ -2,8 +2,9 @@ import pandas as pd
 from yandexfreetranslate import YandexFreeTranslate 
 import requests
 
+
 def get_quotes(num_quotes):
-    #get quotes from API
+     '''get quotes from API'''
     quotes = []
     for _ in range(num_quotes):
         response = requests.get("https://favqs.com/api/qotd")
@@ -13,27 +14,31 @@ def get_quotes(num_quotes):
         quotes.append(rus_quote)
     return quotes
 
+
 def save_quotes_to_excel(data, file_path):
-    #write quotes to excel file
+     '''write quotes to excel file'''
     df = pd.DataFrame(data)
     df.to_excel(file_path, index=False, header=False)
-    
+
+
 def save_quotes_to_csv(data, file_path):
-    #write quotes to csv file
+     '''write quotes to csv file'''
     df = pd.DataFrame(data)
     df.to_csv(file_path, index=False, header=False, encoding='utf-8-sig')
 
+
 def save_quotes_to_txt(data, file_path):
-    #write quotes to txt file
+     '''write quotes to txt file'''
     df = pd.DataFrame(data)
     df.to_csv(file_path, index=False, header=False)
 
+
 if __name__ == "__main__":
     try:
-        num_quotes = 10 #quotes quantity
+        num_quotes = 10  '''quotes quantity'''
         quotes = get_quotes(num_quotes)
         
-        #file paths
+         '''file paths'''
         file_path_excel = 'C:/NURSULTAN/Python/wb_tech/50_quotes/pandas_quotes/ex_result.xlsx' 
         file_path_csv = 'C:/NURSULTAN/Python/wb_tech/50_quotes/pandas_quotes/csv_result.csv'
         file_path_txt = 'C:/NURSULTAN/Python/wb_tech/50_quotes/pandas_quotes/txt_result.txt'
